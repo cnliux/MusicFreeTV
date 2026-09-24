b3ff417b1a457ae1ba7057296fad2d239991a96a
你的名字 <cnliux@qq.com>
2026-09-24 17:26:54 +0800

    revert: 移除 FocusTvButton 组件与 tv-material 依赖，首页快捷入口恢复 tvFocus 实现

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,7 +111,6 @@ dependencies {
     implementation(libs.media3.exoplayer.dash)
     implementation(libs.media3.exoplayer.hls)
     implementation(libs.media3.ui)
-    implementation(libs.tv.material)
     implementation(libs.media3.session)
     implementation(libs.media3.common)
 
